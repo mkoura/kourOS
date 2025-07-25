04895ded485b12802369c02d520e5935a0af3822
Martin Kourim <martin.kourim@iohk.io>
2025-07-25 13:08:49 +0200

    fix(cleanup): fix `Device or resource busy`

--- build_files/build.sh
+++ build_files/build.sh
@@ -41,15 +41,6 @@ install -d -m 0755 /nix
 
 # Clean package manager cache
 dnf5 clean all
-
-# Clean temporary files
-rm -rf /tmp/*
-# shellcheck disable=SC2115
-rm -rf /var/*
-rm -rf /usr/etc
-
-# Restore and setup directories
-mkdir -p /tmp
-install -d /var/tmp -m 1777 /var/tmp
+rm -rf /var/lib/dnf
 
 # }}}
